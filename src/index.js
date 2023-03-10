cbed25f85bcbadb17eafc4d0b1fcae2949f8842d
Marcos Ricardo <mricardoalves99@gmail.com>
2023-03-10 18:11:37 -0300

    refactor: create api folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-
 const express = require('express')
-const routes = require('../routes')
+const about = require('./api/about')
 
 const app = express()
 const PORT = 4000
 
-app.use('/', routes);
+app.use('/api/about', about);
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const AboutController = require('./controllers/AboutController');
-
-routes.get('/about', AboutController.index)
-
-module.exports = routes;
